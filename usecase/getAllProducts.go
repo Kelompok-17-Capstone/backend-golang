10a7fc4fffb9f561d5ac8a037604288136ae699c
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-05-25 20:00:17 +0700

    membuat logic get all product

---
+++ usecase/getAllProducts.go
@@ -0,0 +1,36 @@
+package usecase
+
+import (
+	"backend-golang/models/payload"
+	"backend-golang/repository/database"
+)
+
+// import (
+// 	"backend-golang/models"
+// 	"backend-golang/repository/database"
+// )
+
+// func GetAllProduct() ([]models.Product, error) {
+// 	products, err := database.GetAllProduct()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return products, nil
+// }
+
+func GetAllProduct() (resp []payload.ProductResponse, err error) {
+	products, err := database.GetAllProduct()
+	if err != nil {
+		return []payload.ProductResponse{}, err
+	}
+	for _, product := range products {
+		resp = append(resp, payload.ProductResponse{
+			ProductName: product.ProductName,
+			Description: product.Description,
+			Stock:       product.Stock,
+			Price:       product.Price,
+			Status:      product.Status,
+		})
+	}
+	return
+}
