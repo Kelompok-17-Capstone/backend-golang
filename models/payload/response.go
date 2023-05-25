cf6b9fcdd54aa9be5c76280a4689654500142fe3
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-05-25 21:05:34 +0700

    membuat payload get all product

---
+++ models/payload/response.go
@@ -0,0 +1,12 @@
+package payload
+
+import uuid "github.com/satori/go.uuid"
+
+type ProductResponse struct {
+	ID          uuid.UUID `json:"id" form:"id"`
+	Name        string    `json:"name" form:"name"`
+	Description string    `json:"description" form:"description"`
+	Stock       uint      `json:"stock" form:"stock"`
+	Price       uint      `json:"price" form:"price"`
+	Image       string    `json:"image" form:"image"`
+}
