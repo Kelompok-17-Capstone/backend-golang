7d9c46a395f1affe68ac6c6dffade370914cb223
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-05-26 22:30:55 +0700

    membuat controller get product by id

--- controllers/getAllProducts.go
+++ controllers/getAllProducts.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	uuid "github.com/satori/go.uuid"
 )
 
 // get all product
@@ -21,4 +22,19 @@ func GetAllProductController(c echo.Context) error {
 		"message":  "success get all product",
 		"products": products,
 	})
+}
+
+// get product by ID
+
+func GetProductByIDController(c echo.Context) error {
+	id := c.Param("id")
+	product, err := usecase.GetProductByid(uuid.FromStringOrNil(id))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get item",
+		"data":    product,
+	})
+
 }
