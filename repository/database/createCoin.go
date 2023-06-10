eb58116daa0ba3381d267e3ca6bbc316d6ae0ed7
Aryo Bimo <abimo2018@gmail.com>
2023-06-10 19:02:30 +0700

    merubah models balance dan memperbaiki logic top up

--- repository/database/createCoin.go
+++ repository/database/createCoin.go
@@ -11,11 +11,3 @@ func CreateCoin(coin *models.Coin) error {
 	}
 	return nil
 }
-
-func UpdateCoin(id uint, total uint) error {
-	var coin = models.Coin{}
-	if err := config.DB.Model(&coin).Where("id = ?", id).Update("total", total).Error; err != nil {
-		return err
-	}
-	return nil
-}
