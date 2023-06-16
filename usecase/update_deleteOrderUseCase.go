884df60a9ebca27e1b1448370aec6b32a9e978a5
ravitasmii <115474684+RavitaNRL@users.noreply.github.com>
2023-06-16 21:09:01 +0700

    merubah usecase updaeOrder

--- usecase/update_deleteOrderUseCase.go
+++ usecase/update_deleteOrderUseCase.go
@@ -8,24 +8,60 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-// update status order
-func UpdateStatusOrder(id uuid.UUID, req *payload.UpdateOrderStatus) error {
-	if err := database.UpdateStatusOrder(id, req.Status); err != nil {
+// delete order by id
+func DeleteOrder(id uuid.UUID) error {
+	order, err := database.GetOrderByID(id)
+	if err != nil {
+		return err
+	}
+
+	if err := database.DeleteOrderDetailByOrderID(order.ID); err != nil {
+		return err
+	}
+
+	if err := database.DeleteOrder(order); err != nil {
 		return err
 	}
 	return nil
 }
 
-// update arrived at order
-func UpdateArrivedAtOrder(id uuid.UUID, req *payload.UpadateArrivedAt) error {
+// update order by ID
+func UpdateOrderStatusAndArrived(id uuid.UUID, req *payload.UpdateOrder) error {
 	arrivedAt, err := time.Parse(time.RFC3339, req.ArrivedAt)
 	if err != nil {
 		return err
 	}
 
-	if err := database.UpdateArrivedAtOrder(id, arrivedAt); err != nil {
+	if err := database.UpdateOrderStatusAndArrived(id, req.Status, arrivedAt); err != nil {
 		return err
 	}
 	return nil
+}
 
+// get order by id
+func GetOrderByID(id uuid.UUID) (*payload.GetOrders, error) {
+	order, err := database.GetOrderByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	var qty int
+	var product []string
+	for _, val := range order.OrderDetails {
+		qty += val.Quantity
+		product = append(product, val.Product.Name)
+
+	}
+
+	respon := &payload.GetOrders{
+		Name:          order.User.Profile.Name,
+		Address:       order.Address,
+		TotalQuantity: qty,
+		TotalPrice:    order.GrandTotalPrice,
+		OrderAt:       order.OrderAt,
+		ArrivedAt:     order.ArrivedAt,
+		Status:        order.Status,
+	}
+
+	return respon, nil
 }
